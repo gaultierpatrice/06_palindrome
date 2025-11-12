a28b6ee6c31197ea1f78a628b7580285b81e0443
Gaultier Patrice <gaultiercode@gmail.com>
2025-11-12 16:01:03 +0100

    check if palindrome ok

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 const maxDays = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-let userDate = "31/08/1951"
+let userDate = "10/02/2001"
 
-function isValidDate() {
+function isValidDate(userDate) {
   const parts = userDate.split("/")
   const day = parseInt(parts[0], 10)
   const month = parseInt(parts[1], 10)
+  const year = parseInt(parts[2], 10)
 
-  console.log(month)
+  if (day <= 0) {
+    console.log("The date is incorrect")
+    return
+  }
 
   if (day <= maxDays[month]) {
     console.log("Date is ok")
+    isPalindrome(userDate)
   } else {
-    console.log("Bad Date")
+    console.log("The date is incorrect")
+    return
+  }
+}
+
+function isPalindrome(userDate) {
+  const date = userDate.replace(/\D/g, "")
+  const reverseDate = date.split("").reverse().join("")
+
+  if (date === reverseDate) {
+    console.log("it's a palindrome")
+    findNextPalindrome(userDate)
+  } else {
+    console.log("it's not a palindrome")
+  }
+}
+
+function findNextPalindrome() {
+  let nextDate = userDate
+  nextDate++
+
+  if (date === reverseDate) {
+    console.log("it's a palindrome")
+  } else {
+    console.log("it's not a palindrome")
   }
 }
 
